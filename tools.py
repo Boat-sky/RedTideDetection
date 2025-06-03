dc2c24b46ef087afb47315cf05730c0ba1421dea
Nuttaset <59770656+Boat-sky@users.noreply.github.com>
2025-06-04 05:25:30 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -104,3 +104,71 @@ def GridImages2Array(image, grid_gdf):
       grid_arrays[cell_idx] = np.transpose(cell_array, (2,0,1))
     
     return grid_arrays, grid_coords
+    
+def z_score_norm(img):
+        normalized = np.zeros_like(img, dtype=np.float32)
+        mean_vals = [442.82249157627206, 420.2092339188718, 237.88048782700525, 241.14693390215567, 188.6111751669924, 185.5185900821598, 156.0033714034493, 158.70546912717577]
+        sd_vals = [424.30795110238336, 403.5894175859025, 309.6133615288345, 313.5777765725138, 269.1745800185496, 257.17273952234876, 254.55214828171793, 234.00751552086538]
+        
+        for i in range(img.shape[0]):
+            normalized[i] = (img[i] - mean_vals[i]) / sd_vals[i]
+
+        return normalized
+
+def img_preprocessing(img):
+    # if img.dtype == "uint8":
+    #     img = img / 255.
+    # elif img.dtype == "uint16":
+    #     img = img / 65535.
+
+    img = z_score_norm(img)
+    return img
+
+def DetectOneGrid(input_img, model):
+  img = input_img[:-1, :, :].copy()
+  colap_size = 20
+  crop_size = 512
+  img = torch.from_numpy(img_preprocessing(img)).float()
+  c, h, w = img.shape
+  sum_prop = torch.zeros(h, w, device=device)
+  predict_count = torch.zeros(h, w, device=device)
+
+  for i in range(0, h, crop_size-colap_size):
+    if i + crop_size > h:
+        i = h - crop_size
+    for j in range(0, w, crop_size-colap_size):
+        if j + crop_size > w:
+            j = w - crop_size
+
+        cropped_img = img[:, i:i+crop_size, j:j+crop_size]
+
+        tensor_img = torch.tensor(cropped_img).float().to(device)
+        tensor_img = tensor_img.unsqueeze(0)
+
+        with torch.no_grad():
+            probs = model(tensor_img)
+
+        sum_prop[i:i+crop_size, j:j+crop_size] += probs.squeeze(0).squeeze(0)
+        predict_count[i:i+crop_size, j:j+crop_size] += 1
+
+  big_img_probs = sum_prop / predict_count
+  big_img_mask = (big_img_probs > 0.5).float().cpu().numpy()
+  return big_img_mask
+
+def DetectRedTide(grid_arrays, model):
+  model.to(device)
+  model.eval()
+
+  result_arrays = {}
+  for k, v in grid_arrays.items():
+    # if 200 in v[10]:
+    #   black_array = np.zeros(v[0].shape)
+    #   result_arrays[k] = black_array
+    # else:
+    #   result_arrays[k] = DetectOneGrid(v, model)
+    prediction = DetectOneGrid(np.where(np.isinf(v), 0., v), model)
+    inf_mask = np.isinf(v).any(axis=0)
+
+    prediction[inf_mask] = 0
+    result_arrays[k] = prediction
+  return result_arrays
